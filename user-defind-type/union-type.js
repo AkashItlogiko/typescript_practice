7f3bbe912e397454d345a1839b8f04711cf3314b
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-21 16:09:21 +0600

    Done

---
+++ user-defind-type/union-type.js
@@ -0,0 +1,9 @@
+// let userId: string | number | boolean;
+// userId = '120';
+// userId = 101;
+// userId = true;
+function display(userId) {
+    console.log(userId);
+}
+display('101');
+display(102);
---
+++ user-defind-type/union-type.ts
@@ -0,0 +1,11 @@
+// let userId: string | number | boolean;
+
+// userId = '120';
+// userId = 101;
+// userId = true;
+
+function display(userId: string | number) {
+  console.log(userId);
+}
+display('101');
+display(102);
